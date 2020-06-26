2eb454c2d7857a17b713c9c8f22e9c1672886418
Davidstephenss <davidstephens@Davids-MacBook-Air.local>
2020-06-26 14:46:35 -0500

    filtersDONE

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // We'll pre-populate this array with a couple objects just so it's not undefined if your internet connection isn't working properly.
-
+// let userID = null
 let arrayOfTodos = [
     {
     "userId": 14,
@@ -15,20 +15,31 @@ let arrayOfTodos = [
   }]
 
   let fontColor = ""
+
   const fetchTodos = () => {
     fetch('https://jsonplaceholder.typicode.com/todos')
     .then( (response) => response.json())
     .then( (json) => arrayOfTodos = json)
   }
+  
+  const userNum = (num) => {
+    x = parseInt (num)
+    console.log(num)
+  } 
 
   const logTodos = () => {
     console.log(arrayOfTodos)
   }
 
-  const populateTodos = () => {
+  let x=""
+
+  const populateAllTodos = () => {
+        populateTodos(arrayOfTodos)} 
+
+  const populateTodos = (arr) => {
       let newElement = document.getElementById("todo-list")
     
-      for (let i = 0; i < arrayOfTodos.length; i++){
+      for (let i = 0; i < arr.length; i++){
         
         let newLi = document.createElement('Li')
         let newH1 = document.createElement('H1')
@@ -37,13 +48,13 @@ let arrayOfTodos = [
         let newH4 = document.createElement('H4')
 
 
-        let userId = document.createTextNode(arrayOfTodos[i].userId)
+        let userId = document.createTextNode(arr[i].userId)
         newH1.appendChild(userId)
-        let id = document.createTextNode(arrayOfTodos[i].id)
+        let id = document.createTextNode(arr[i].id)
         newH2.appendChild(id)
-        let title = document.createTextNode(arrayOfTodos[i].title)
+        let title = document.createTextNode(arr[i].title)
         newH3.appendChild(title)
-        let completed = document.createTextNode(arrayOfTodos[i].completed)
+        let completed = document.createTextNode(arr[i].completed)
         newH4.appendChild(completed)
 
         newElement.appendChild(newLi)
@@ -52,7 +63,7 @@ let arrayOfTodos = [
         newLi.appendChild(newH3)
         newLi.appendChild(newH4)
 
-        if (arrayOfTodos[i].completed === false) {
+        if (arr[i].completed === false) {
           fontColor = 'green'
         } else {
           fontColor = 'red'
@@ -60,3 +71,47 @@ let arrayOfTodos = [
       newH4.style.color=fontColor
   }
 }
+
+// fetch same data
+
+//store in a variable
+
+// add input to take in 1-10
+
+//add buttons for clear, populate 
+
+
+// const storeID = (value) => {
+//   userID = value 
+// }
+
+const showUserID = () => {
+let result = arrayOfTodos.filter(function(numID) {
+  return numID.userId === x
+  })
+  populateTodos(result)
+}
+
+const showCompleted = () => {
+ let arrayOfCompletedTodos = arrayOfTodos.filter(function(todo) {
+  return todo.completed === true  
+  })
+  populateTodos(arrayOfCompletedTodos)
+}
+
+const showUncompleted = () => {
+  let arrayOfUncompletedTodos = arrayOfTodos.filter(function(todo) {
+   return todo.completed === false  
+   })
+   populateTodos(arrayOfUncompletedTodos)
+ }
+
+
+ const clearList = () => {
+  const arrayOfTodos=document.getElementsByTagName("OL")
+  for (i=0; i < arrayOfTodos.length; i++)
+  arrayOfTodos[i].innerHTML=null
+ }
+
+ //VS code sucks. I did my best lol.//
+ 
